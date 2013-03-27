1cb3f21f9adf6dd1adc80d0db9e67db0b879f962
Alexander Bluhm <alexander.bluhm@gmx.net>
2013-03-27 16:41:22 +0100

    cvs commit

--- kern/uipc_socket.c
+++ kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket.c,v 1.115 2013/03/19 20:07:14 bluhm Exp $	*/
+/*	$OpenBSD: uipc_socket.c,v 1.116 2013/03/27 15:41:04 bluhm Exp $	*/
 /*	$NetBSD: uipc_socket.c,v 1.21 1996/02/04 02:17:52 christos Exp $	*/
 
 /*
