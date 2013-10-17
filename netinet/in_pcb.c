499a686ac9c4156b6b91857a42b73fb68302f116
Alexander Bluhm <alexander.bluhm@gmx.net>
2013-10-17 18:57:14 +0200

    use LIST_FOREACH in in_pcblookup

--- netinet/in_pcb.c
+++ netinet/in_pcb.c
@@ -658,8 +658,7 @@ in_pcblookup(struct inpcbtable *table, void *faddrp, u_int fport_arg,
 	struct in_addr laddr = *(struct in_addr *)laddrp;
 
 	rdomain = rtable_l2(rdomain);	/* convert passed rtableid to rdomain */
-	for (inp = LIST_FIRST(INPCBLHASH(table, lport, rdomain)); inp;
-	    inp = LIST_NEXT(inp, inp_lhash)) {
+	LIST_FOREACH(inp, INPCBLHASH(table, lport, rdomain), inp_lhash) {
 		if (rtable_l2(inp->inp_rtableid) != rdomain)
 			continue;
 		if (inp->inp_lport != lport)
