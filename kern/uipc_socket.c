7c04081a62af14df55e008be17a0bbd859488b27
Alexander Bluhm <alexander.bluhm@gmx.net>
2013-12-31 16:41:45 +0100

    reorder variable definitions

--- kern/uipc_socket.c
+++ kern/uipc_socket.c
@@ -1197,9 +1197,8 @@ somove(struct socket *so, int wait)
 	struct timeval	 splicetv;
 	struct socket	*sosp = so->so_splice;
 	struct mbuf	*m, **mp, *nextrecord;
-	long long	 usec;
+	long long	 space, usec;
 	u_long		 len, off, oobmark;
-	long long	 space;
 	int		 error = 0, maxreached = 0;
 	short		 state;
 
