2514884d5a9dcb6b8c6953701fd7c2ff38fb063d
Alexander Bluhm <alexander.bluhm@gmx.net>
2014-10-30 19:47:06 +0100

    Do not cast somove() to void.

--- kern/uipc_socket.c
+++ kern/uipc_socket.c
@@ -1189,7 +1189,7 @@ sotask(void *arg1, void *arg2)
 		 * called from softnet interrupt context.  This would remove
 		 * the socket during somove().
 		 */
-		(void) somove(so, M_DONTWAIT);
+		somove(so, M_DONTWAIT);
 	}
 	splx(s);
 
@@ -1485,7 +1485,7 @@ sorwakeup(struct socket *so)
 		if (so->so_proto->pr_flags & PR_WANTRCVD)
 			task_add(sosplice_taskq, &so->so_splicetask);
 		else
-			(void) somove(so, M_DONTWAIT);
+			somove(so, M_DONTWAIT);
 	}
 	if (isspliced(so))
 		return;
