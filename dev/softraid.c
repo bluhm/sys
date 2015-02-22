cfd0028dbe892e1b9f8405476a82a1daa822bf3b
Alexander Bluhm <alexander.bluhm@gmx.net>
2015-02-22 23:02:07 +0100

    Replace SLIST_FOREACH/SLIST_REMOVE with SLIST_EMPTY/SLIST_REMOVE_HEAD.

--- dev/softraid.c
+++ dev/softraid.c
@@ -263,8 +263,8 @@ sr_meta_attach(struct sr_discipline *sd, int chunk_no, int force)
 		goto bad;
 
 	/* Force chunks into correct order now that metadata is attached. */
-	SLIST_FOREACH(ch_entry, cl, src_link)
-		SLIST_REMOVE(cl, ch_entry, sr_chunk, src_link);
+	while (!SLIST_EMPTY(cl))
+		SLIST_REMOVE_HEAD(cl, src_link);
 	for (i = 0; i < chunk_no; i++) {
 		ch_entry = sd->sd_vol.sv_chunks[i];
 		chunk2 = NULL;
