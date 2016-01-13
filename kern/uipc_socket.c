ab304c0f34c39d51179937440150941b478b3d56
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-01-13 22:39:53 +0100

    cvs commit

--- kern/uipc_socket.c
+++ kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket.c,v 1.145 2016/01/06 10:06:50 stefan Exp $	*/
+/*	$OpenBSD: uipc_socket.c,v 1.146 2016/01/13 21:39:39 bluhm Exp $	*/
 /*	$NetBSD: uipc_socket.c,v 1.21 1996/02/04 02:17:52 christos Exp $	*/
 
 /*
