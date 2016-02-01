4e5f35db381ba1aebcceae1fd00c469ab4dbd1f5
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-02-01 15:49:05 +0100

    Remove VOP_CLOSE error in ext2fs_unmount().

--- ufs/ext2fs/ext2fs_vfsops.c
+++ ufs/ext2fs/ext2fs_vfsops.c
@@ -636,15 +636,15 @@ ext2fs_unmount(struct mount *mp, int mntflags, struct proc *p)
 	if (ump->um_devvp->v_type != VBAD)
 		ump->um_devvp->v_specmountpoint = NULL;
 	vn_lock(ump->um_devvp, LK_EXCLUSIVE | LK_RETRY, p);
-	error = VOP_CLOSE(ump->um_devvp, fs->e2fs_ronly ? FREAD : FREAD|FWRITE,
+	VOP_CLOSE(ump->um_devvp, fs->e2fs_ronly ? FREAD : FREAD|FWRITE,
 	    NOCRED, p);
 	vput(ump->um_devvp);
 	free(fs->e2fs_gd, M_UFSMNT, gdescs_space);
 	free(fs, M_UFSMNT, sizeof *fs);
 	free(ump, M_UFSMNT, sizeof *ump);
-	mp->mnt_data = (qaddr_t)0;
+	mp->mnt_data = NULL;
 	mp->mnt_flag &= ~MNT_LOCAL;
-	return (error);
+	return (0);
 }
 
 /*
