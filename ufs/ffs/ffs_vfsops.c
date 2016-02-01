7d1060b6c0e0c33af76e3d5438082975e1c9b91c
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-02-01 15:50:17 +0100

    Remove VOP_CLOSE error in ffs_unmount().

--- ufs/ffs/ffs_vfsops.c
+++ ufs/ffs/ffs_vfsops.c
@@ -1022,15 +1022,15 @@ ffs_unmount(struct mount *mp, int mntflags, struct proc *p)
 
 	vn_lock(ump->um_devvp, LK_EXCLUSIVE | LK_RETRY, p);
 	vinvalbuf(ump->um_devvp, V_SAVE, NOCRED, p, 0, 0);
-	error = VOP_CLOSE(ump->um_devvp, fs->fs_ronly ? FREAD : FREAD|FWRITE,
-		NOCRED, p);
+	VOP_CLOSE(ump->um_devvp, fs->fs_ronly ? FREAD : FREAD|FWRITE,
+	    NOCRED, p);
 	vput(ump->um_devvp);
 	free(fs->fs_csp, M_UFSMNT, 0);
 	free(fs, M_UFSMNT, fs->fs_sbsize);
 	free(ump, M_UFSMNT, sizeof(*ump));
-	mp->mnt_data = (qaddr_t)0;
+	mp->mnt_data = NULL;
 	mp->mnt_flag &= ~MNT_LOCAL;
-	return (error);
+	return (0);
 }
 
 /*
