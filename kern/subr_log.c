d406729d730cf3ef5e518187ce8fc64f73360ed5
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-05-19 01:28:28 +0200

    cvs commit

--- kern/subr_log.c
+++ kern/subr_log.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: subr_log.c,v 1.41 2016/05/17 23:43:47 bluhm Exp $	*/
+/*	$OpenBSD: subr_log.c,v 1.42 2016/05/18 23:28:43 bluhm Exp $	*/
 /*	$NetBSD: subr_log.c,v 1.11 1996/03/30 22:24:44 christos Exp $	*/
 
 /*
