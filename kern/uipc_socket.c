9f9134db97aa3330de3ca954b1298dd07ceb56d9
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-08-10 14:13:18 +0200

    Revert "Bring back the (void) casts to the somove() calls."
    This reverts commit 3b12ad2de963f76c50e1cf52832de84e01015eb9.

--- kern/uipc_socket.c
+++ kern/uipc_socket.c
@@ -1195,7 +1195,7 @@ sotask(void *arg)
 		 * called from softnet interrupt context.  This would remove
 		 * the socket during somove().
 		 */
-		(void) somove(so, M_DONTWAIT);
+		somove(so, M_DONTWAIT);
 	}
 	splx(s);
 
@@ -1494,7 +1494,7 @@ sorwakeup(struct socket *so)
 		if (so->so_proto->pr_flags & PR_WANTRCVD)
 			task_add(sosplice_taskq, &so->so_splicetask);
 		else
-			(void) somove(so, M_DONTWAIT);
+			somove(so, M_DONTWAIT);
 	}
 	if (isspliced(so))
 		return;
