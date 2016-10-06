a23590e2d1af122bd2c266d9c5c1940130283297
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-10-06 18:58:07 +0200

    Do not call splsoftnet() from sowakeup().

--- kern/uipc_socket2.c
+++ kern/uipc_socket2.c
@@ -315,7 +315,7 @@ sbunlock(struct sockbuf *sb)
 void
 sowakeup(struct socket *so, struct sockbuf *sb)
 {
-	int s = splsoftnet();
+	splsoftassert(IPL_SOFTNET);
 
 	selwakeup(&sb->sb_sel);
 	sb->sb_flagsintr &= ~SB_SEL;
@@ -323,7 +323,6 @@ sowakeup(struct socket *so, struct sockbuf *sb)
 		sb->sb_flagsintr &= ~SB_WAIT;
 		wakeup(&sb->sb_cc);
 	}
-	splx(s);
 	if (so->so_state & SS_ASYNC)
 		csignal(so->so_pgid, SIGIO, so->so_siguid, so->so_sigeuid);
 }
