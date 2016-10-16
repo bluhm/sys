c28752d8bcd190ea8e2ce6965dc2f92a96de24d0
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-10-16 23:45:19 +0200

    cvs commit

--- net/if.c
+++ net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.454 2016/10/09 20:05:10 claudio Exp $	*/
+/*	$OpenBSD: if.c,v 1.455 2016/10/16 21:45:17 bluhm Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
