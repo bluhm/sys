4c6f656502fd54b1a6b1c7f37519ef5d71869eee
Alexander Bluhm <alexander.bluhm@gmx.net>
2016-10-26 15:57:14 +0200

    Apply genuos 7da7ce32.diff.

--- net/pf.c
+++ net/pf.c
@@ -5859,6 +5859,14 @@ pf_route(struct mbuf **m, struct pf_rule *r, int dir, struct ifnet *oifp,
 
 		m0->m_pkthdr.pf.flags |= PF_TAG_GENERATED;
 	} else {
+		if (ip->ip_ttl <= IPTTLDEC) {
+			if (r->rt != PF_DUPTO) {
+				icmp_error(m0, ICMP_TIMXCEED,
+				    ICMP_TIMXCEED_INTRANS, 0, 0);
+				goto done;
+			}
+		}
+		ip->ip_ttl -= IPTTLDEC;
 		if (s == NULL) {
 			bzero(sns, sizeof(sns));
 			if (pf_map_addr(AF_INET, r,
@@ -6016,6 +6024,15 @@ pf_route6(struct mbuf **m, struct pf_rule *r, int dir, struct ifnet *oifp,
 		ip6_output(m0, NULL, NULL, 0, NULL, NULL);
 		return;
 	}
+
+	if (ip6->ip6_hlim <= IPV6_HLIMDEC) {
+		if (r->rt != PF_DUPTO) {
+			icmp6_error(m0, ICMP6_TIME_EXCEEDED,
+			    ICMP6_TIME_EXCEED_TRANSIT, 0);
+			goto done;
+		}
+	}
+	ip6->ip6_hlim -= IPV6_HLIMDEC;
 
 	if (s == NULL) {
 		bzero(sns, sizeof(sns));
