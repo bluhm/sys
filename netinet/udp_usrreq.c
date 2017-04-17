b8fc5aa8d1b8a41037bf0a02830ed61ff0fc3b47
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-04-17 22:47:17 +0200

    cvs commit

--- netinet/udp_usrreq.c
+++ netinet/udp_usrreq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: udp_usrreq.c,v 1.234 2017/04/14 20:46:31 bluhm Exp $	*/
+/*	$OpenBSD: udp_usrreq.c,v 1.235 2017/04/17 20:48:21 bluhm Exp $	*/
 /*	$NetBSD: udp_usrreq.c,v 1.28 1996/03/16 23:54:03 christos Exp $	*/
 
 /*
