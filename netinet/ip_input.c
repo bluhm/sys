6f90e83ca821036c3703c53e8a16229877c3a01b
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-05-15 01:37:37 +0200

    Rename ipv4_input() to ip_input().

--- netinet/ip_input.c
+++ netinet/ip_input.c
@@ -227,7 +227,7 @@ ipintr(void)
 			panic("ipintr no HDR");
 #endif
 		off = 0;
-		ipv4_input(&m, &off, IPPROTO_IPV4, AF_UNSPEC);
+		ip_input(&m, &off, IPPROTO_IPV4, AF_UNSPEC);
 	}
 }
 
@@ -237,7 +237,7 @@ ipintr(void)
  * Checksum and byte swap header.  Process options. Forward or deliver.
  */
 int
-ipv4_input(struct mbuf **mp, int *offp, int nxt, int af)
+ip_input(struct mbuf **mp, int *offp, int nxt, int af)
 {
 	struct mbuf	*m = *mp;
 	struct ifnet	*ifp;
