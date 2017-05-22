c3d8c6089624b966e0351dba1297db8028e3706f
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-05-22 16:26:20 +0200

    cvs commit

--- netinet/ip_icmp.c
+++ netinet/ip_icmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_icmp.c,v 1.167 2017/05/04 17:58:46 bluhm Exp $	*/
+/*	$OpenBSD: ip_icmp.c,v 1.168 2017/05/22 14:26:08 bluhm Exp $	*/
 /*	$NetBSD: ip_icmp.c,v 1.19 1996/02/13 23:42:22 christos Exp $	*/
 
 /*
