b455196bbf4afa33aa2925d03d8db3c62b0cd195
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-05-27 16:17:22 +0200

    Before accessing m_pkthdr check M_PKTHDR flag.

--- kern/uipc_mbuf.c
+++ kern/uipc_mbuf.c
@@ -519,10 +519,7 @@ m_defrag(struct mbuf *m, int how)
 	if (m->m_next == NULL)
 		return (0);
 
-#ifdef DIAGNOSTIC
-	if (!(m->m_flags & M_PKTHDR))
-		panic("m_defrag: no packet hdr or not a chain");
-#endif
+	KASSERT(m->m_flags & M_PKTHDR);
 
 	if ((m0 = m_gethdr(how, m->m_type)) == NULL)
 		return (ENOBUFS);
@@ -1078,6 +1075,7 @@ m_makespace(struct mbuf *m0, int skip, int hlen, int *off)
 	struct mbuf *m;
 	unsigned remain;
 
+	KASSERT(m0->m_flags & M_PKTHDR);
 	/*
 	 * Limit the size of the new header to MHLEN. In case
 	 * skip = 0 and the first buffer is not a cluster this
@@ -1342,6 +1340,8 @@ m_dup_pkt(struct mbuf *m0, unsigned int adj, int wait)
 {
 	struct mbuf *m;
 	int len;
+
+	KASSERT(m0->m_flags & M_PKTHDR);
 
 	len = m0->m_pkthdr.len + adj;
 	if (len > MAXMCLBYTES) /* XXX */
