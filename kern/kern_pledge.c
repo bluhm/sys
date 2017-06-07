f3030da3a2e824905278bd35cf24c02999a2572b
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-06-07 12:45:52 +0200

    Set the pledge accounting flag.

--- kern/kern_pledge.c
+++ kern/kern_pledge.c
@@ -32,6 +32,7 @@
 #include <sys/mman.h>
 #include <sys/sysctl.h>
 #include <sys/ktrace.h>
+#include <sys/acct.h>
 
 #include <sys/ioctl.h>
 #include <sys/termios.h>
@@ -581,6 +582,7 @@ pledge_fail(struct proc *p, int error, uint64_t code)
 		}
 	printf("%s(%d): syscall %d \"%s\"\n", p->p_p->ps_comm, p->p_p->ps_pid,
 	    p->p_pledge_syscall, codes);
+	p->p_p->ps_acflag |= APLEDGE;
 #ifdef KTRACE
 	if (KTRPOINT(p, KTR_PLEDGE))
 		ktrpledge(p, error, code, p->p_pledge_syscall);
