a5463eb5c0319617671a6cbc3b778662e1d11c53
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-06-20 19:06:01 +0200

    Print so_state in hex in so_print().

--- kern/uipc_socket.c
+++ kern/uipc_socket.c
@@ -2051,7 +2051,7 @@ so_print(void *v,
 	(*pr)("so_type: %i\n", so->so_type);
 	(*pr)("so_options: 0x%04x\n", so->so_options); /* %b */
 	(*pr)("so_linger: %i\n", so->so_linger);
-	(*pr)("so_state: %i\n", so->so_state);
+	(*pr)("so_state: 0x%04x\n", so->so_state);
 	(*pr)("so_pcb: %p\n", so->so_pcb);
 	(*pr)("so_proto: %p\n", so->so_proto);
 
