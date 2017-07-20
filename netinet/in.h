790e0adb3916ccae8ed409ade7bbad241bc98632
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-07-21 00:14:27 +0200

    Implement nam2sin functions.

--- netinet/in.h
+++ netinet/in.h
@@ -818,6 +818,7 @@ void	   in_proto_cksum_out(struct mbuf *, struct ifnet *);
 void	   in_ifdetach(struct ifnet *);
 int	   in_mask2len(struct in_addr *);
 void	   in_len2mask(struct in_addr *, int);
+int	   in_nam2sin(struct sockaddr_in **, const struct mbuf *);
 
 char	  *inet_ntoa(struct in_addr);
 int	   inet_nat64(int, const void *, void *, const void *, u_int8_t);
