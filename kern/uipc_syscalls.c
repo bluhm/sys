0bb33c6809b1fb8e29a17a4b42ddd81ee2137ff4
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-07-20 20:40:46 +0200

    cvs commit

--- kern/uipc_syscalls.c
+++ kern/uipc_syscalls.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_syscalls.c,v 1.154 2017/07/19 06:52:41 claudio Exp $	*/
+/*	$OpenBSD: uipc_syscalls.c,v 1.155 2017/07/20 18:40:16 bluhm Exp $	*/
 /*	$NetBSD: uipc_syscalls.c,v 1.19 1996/02/09 19:00:48 christos Exp $	*/
 
 /*
