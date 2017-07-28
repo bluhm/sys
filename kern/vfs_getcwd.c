15f6bbe22e66c06b0b69dda070f16819d55245cc
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-07-28 23:55:01 +0200

    cvs commit

--- kern/vfs_getcwd.c
+++ kern/vfs_getcwd.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: vfs_getcwd.c,v 1.26 2016/03/19 12:04:15 natano Exp $ */
+/* $OpenBSD: vfs_getcwd.c,v 1.27 2017/07/28 21:54:49 bluhm Exp $ */
 /* $NetBSD: vfs_getcwd.c,v 1.3.2.3 1999/07/11 10:24:09 sommerfeld Exp $ */
 
 /*
