7720e7a1746b73e998ad986ccaed3589f0931b95
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-10-12 20:18:47 +0200

    Reset header in icmp6 reflect and increase loop count.

--- netinet6/icmp6.c
+++ netinet6/icmp6.c
@@ -1044,6 +1044,7 @@ icmp6_reflect(struct mbuf *m, size_t off)
 	struct icmp6_hdr *icmp6;
 	struct in6_addr t, *src = NULL;
 	struct sockaddr_in6 sa6_src, sa6_dst;
+	u_int rtableid;
 
 	CTASSERT(sizeof(struct ip6_hdr) + sizeof(struct icmp6_hdr) <= MHLEN);
 
@@ -1055,6 +1056,11 @@ icmp6_reflect(struct mbuf *m, size_t off)
 		    __FILE__, __LINE__));
 		goto bad;
 	}
+
+	if (m->m_pkthdr.ph_loopcnt++ >= M_MAXLOOP)
+		goto bad;
+	rtableid = m->m_pkthdr.ph_rtableid;
+	m_resethdr(m);
 
 	/*
 	 * If there are extra headers between IPv6 and ICMPv6, strip
@@ -1114,7 +1120,7 @@ icmp6_reflect(struct mbuf *m, size_t off)
 	 * but is possible (for example) when we encounter an error while
 	 * forwarding procedure destined to a duplicated address of ours.
 	 */
-	rt = rtalloc(sin6tosa(&sa6_dst), 0, m->m_pkthdr.ph_rtableid);
+	rt = rtalloc(sin6tosa(&sa6_dst), 0, rtableid);
 	if (rtisvalid(rt) && ISSET(rt->rt_flags, RTF_LOCAL) &&
 	    !ISSET(ifatoia6(rt->rt_ifa)->ia6_flags,
 	    IN6_IFF_ANYCAST|IN6_IFF_TENTATIVE|IN6_IFF_DUPLICATED)) {
@@ -1129,8 +1135,7 @@ icmp6_reflect(struct mbuf *m, size_t off)
 		 * that we do not own.  Select a source address based on the
 		 * source address of the erroneous packet.
 		 */
-		rt = rtalloc(sin6tosa(&sa6_src), RT_RESOLVE,
-		    m->m_pkthdr.ph_rtableid);
+		rt = rtalloc(sin6tosa(&sa6_src), RT_RESOLVE, rtableid);
 		if (!rtisvalid(rt)) {
 			char addr[INET6_ADDRSTRLEN];
 
@@ -1168,9 +1173,6 @@ icmp6_reflect(struct mbuf *m, size_t off)
 	 * Note that only echo and node information replies are affected,
 	 * since the length of ICMP6 errors is limited to the minimum MTU.
 	 */
-#if NPF > 0
-	pf_pkt_addr_changed(m);
-#endif
 	ip6_send(m);
 	return;
 
