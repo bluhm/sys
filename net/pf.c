0111e57f20798d0fcc64e8a46b8a30a4b5aed5a0
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-01-11 03:27:48 +0100

    Move decrement check down as we have to adjust the address.

--- net/pf.c
+++ net/pf.c
@@ -5912,16 +5912,6 @@ pf_route(struct pf_pdesc *pd, struct pf_rule *r, struct pf_state *s)
 	dst->sin_addr = ip->ip_dst;
 	rtableid = m0->m_pkthdr.ph_rtableid;
 
-	if (pd->dir == PF_IN) {
-		if (ip->ip_ttl <= IPTTLDEC) {
-			if (r->rt != PF_DUPTO)
-				pf_send_icmp(m0, ICMP_TIMXCEED,
-				    ICMP_TIMXCEED_INTRANS, 0,
-				    pd->af, r, pd->rdomain);
-			goto bad;
-		}
-		ip->ip_ttl -= IPTTLDEC;
-	}
 	if (s == NULL) {
 		bzero(sns, sizeof(sns));
 		if (pf_map_addr(AF_INET, r,
@@ -5944,6 +5934,17 @@ pf_route(struct pf_pdesc *pd, struct pf_rule *r, struct pf_state *s)
 	}
 	if (ifp == NULL)
 		goto bad;
+
+	if (pd->dir == PF_IN) {
+		if (ip->ip_ttl <= IPTTLDEC) {
+			if (r->rt != PF_DUPTO)
+				pf_send_icmp(m0, ICMP_TIMXCEED,
+				    ICMP_TIMXCEED_INTRANS, 0,
+				    pd->af, r, pd->rdomain);
+			goto bad;
+		}
+		ip->ip_ttl -= IPTTLDEC;
+	}
 
 	if (pd->kif->pfik_ifp != ifp) {
 		if (pf_test(AF_INET, PF_OUT, ifp, &m0) != PF_PASS)
@@ -6064,16 +6065,6 @@ pf_route6(struct pf_pdesc *pd, struct pf_rule *r, struct pf_state *s)
 	dst->sin6_addr = ip6->ip6_dst;
 	rtableid = m0->m_pkthdr.ph_rtableid;
 
-	if (pd->dir == PF_IN) {
-		if (ip6->ip6_hlim <= IPV6_HLIMDEC) {
-			if (r->rt != PF_DUPTO)
-				pf_send_icmp(m0, ICMP6_TIME_EXCEEDED,
-				    ICMP6_TIME_EXCEED_TRANSIT, 0,
-				    pd->af, r, pd->rdomain);
-			goto bad;
-		}
-		ip6->ip6_hlim -= IPV6_HLIMDEC;
-	}
 	if (s == NULL) {
 		bzero(sns, sizeof(sns));
 		if (pf_map_addr(AF_INET6, r, (struct pf_addr *)&ip6->ip6_src,
@@ -6094,6 +6085,17 @@ pf_route6(struct pf_pdesc *pd, struct pf_rule *r, struct pf_state *s)
 	}
 	if (ifp == NULL)
 		goto bad;
+
+	if (pd->dir == PF_IN) {
+		if (ip6->ip6_hlim <= IPV6_HLIMDEC) {
+			if (r->rt != PF_DUPTO)
+				pf_send_icmp(m0, ICMP6_TIME_EXCEEDED,
+				    ICMP6_TIME_EXCEED_TRANSIT, 0,
+				    pd->af, r, pd->rdomain);
+			goto bad;
+		}
+		ip6->ip6_hlim -= IPV6_HLIMDEC;
+	}
 
 	if (pd->kif->pfik_ifp != ifp) {
 		if (pf_test(AF_INET6, PF_OUT, ifp, &m0) != PF_PASS)
