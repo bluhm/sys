ae174b80b06b1a71d213b40b1cc450b8c6de9ea4
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-01-23 21:50:04 +0100

    cvs commit

--- netinet/tcp_usrreq.c
+++ netinet/tcp_usrreq.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_usrreq.c,v 1.164 2018/01/22 20:27:28 bluhm Exp $	*/
+/*	$OpenBSD: tcp_usrreq.c,v 1.165 2018/01/23 20:49:58 bluhm Exp $	*/
 /*	$NetBSD: tcp_usrreq.c,v 1.20 1996/02/13 23:44:16 christos Exp $	*/
 
 /*
