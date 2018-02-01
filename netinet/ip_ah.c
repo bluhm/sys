262eef66246fddd322030571bba4aad97ea9be71
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-02-01 22:06:37 +0100

    cvs commit

--- netinet/ip_ah.c
+++ netinet/ip_ah.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ah.c,v 1.133 2017/11/08 16:29:20 visa Exp $ */
+/*	$OpenBSD: ip_ah.c,v 1.134 2018/02/01 21:06:31 bluhm Exp $ */
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
