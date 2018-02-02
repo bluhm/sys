26f4007357627117b238db92b1d14957f4d5563b
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-02-02 18:42:02 +0100

    Use flags to keep track of armed timers.

--- netinet/tcp_var.h
+++ netinet/tcp_var.h
@@ -100,6 +100,12 @@ struct tcpcb {
 #define TF_NEEDOUTPUT	0x00800000	/* call tcp_output after tcp_input */
 #define TF_BLOCKOUTPUT	0x01000000	/* avert tcp_output during tcp_input */
 #define TF_NOPUSH	0x02000000	/* don't push */
+#define TF_TMR_REXMT	0x04000000	/* retransmit timer armed */
+#define TF_TMR_PERSIST	0x08000000	/* retransmit persistence timer armed */
+#define TF_TMR_KEEP	0x10000000	/* keep alive timer armed */
+#define TF_TMR_2MSL	0x20000000	/* 2*msl quiet time timer armed */
+#define TF_TMR_REAPER	0x40000000	/* delayed cleanup timer armed, dead */
+#define TF_TIMER	TF_TMR_REXMT	/* used to shift with TCPT values */
 
 	struct	mbuf *t_template;	/* skeletal packet for transmit */
 	struct	inpcb *t_inpcb;		/* back pointer to internet pcb */
