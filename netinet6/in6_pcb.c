5e97b239c02423e8e9e70a5ed08ba9b527bd6d50
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-06-03 21:58:10 +0200

    cvs commit

--- netinet6/in6_pcb.c
+++ netinet6/in6_pcb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in6_pcb.c,v 1.100 2017/08/11 19:53:02 bluhm Exp $	*/
+/*	$OpenBSD: in6_pcb.c,v 1.101 2018/06/03 19:58:02 bluhm Exp $	*/
 
 /*
  * Copyright (C) 1995, 1996, 1997, and 1998 WIDE Project.
