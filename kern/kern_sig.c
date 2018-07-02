86fc97a04531f6cfec24744101446b1dcdab3a8c
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-07-02 12:34:42 +0200

    SIGPENDING() returns signal list.

--- kern/kern_sig.c
+++ kern/kern_sig.c
@@ -1153,8 +1153,7 @@ issignal(struct proc *p)
 	int s;
 
 	for (;;) {
-		mask = p->p_siglist | pr->ps_mainproc->p_siglist;
-		mask &= ~p->p_sigmask;
+		mask = SIGPENDING(p);
 		if (pr->ps_flags & PS_PPWAIT)
 			mask &= ~stopsigmask;
 		if (mask == 0)	 	/* no signal to send */
@@ -1840,7 +1839,7 @@ userret(struct proc *p)
 		KERNEL_UNLOCK();
 	}
 
-	if (SIGPENDING(p)) {
+	if (SIGPENDING(p) != 0) {
 		KERNEL_LOCK();
 		while ((signum = CURSIG(p)) != 0)
 			postsig(p, signum);
