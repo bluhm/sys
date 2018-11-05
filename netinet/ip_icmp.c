7a023808786022d7c859f1d0d0f3b6fc313b5f90
Alexander Bluhm <alexander.bluhm@gmx.net>
2018-11-05 13:52:27 -0700

    cvs commit

--- netinet/ip_icmp.c
+++ netinet/ip_icmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_icmp.c,v 1.178 2018/11/05 10:06:10 claudio Exp $	*/
+/*	$OpenBSD: ip_icmp.c,v 1.179 2018/11/05 20:52:22 bluhm Exp $	*/
 /*	$NetBSD: ip_icmp.c,v 1.19 1996/02/13 23:42:22 christos Exp $	*/
 
 /*
