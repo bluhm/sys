0209642447444da7da35d1d0326b194dd3bd9a1e
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-02-08 13:52:02 +0100

    cvs commit

--- kern/kern_exec.c
+++ kern/kern_exec.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_exec.c,v 1.202 2018/10/30 03:27:45 deraadt Exp $	*/
+/*	$OpenBSD: kern_exec.c,v 1.203 2019/02/08 12:51:57 bluhm Exp $	*/
 /*	$NetBSD: kern_exec.c,v 1.75 1996/02/09 18:59:28 christos Exp $	*/
 
 /*-
