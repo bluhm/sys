8764396366e8cb74199796cb9709e84236195d44
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-04-23 00:16:50 +0200

    Replace u_int in in6_cksum() prototype.

--- netinet6/in6.h
+++ netinet6/in6.h
@@ -413,7 +413,7 @@ struct cmsghdr;
 
 void	ipv6_input(struct ifnet *, struct mbuf *);
 
-int	in6_cksum(struct mbuf *, u_int8_t, u_int32_t, u_int32_t);
+int	in6_cksum(struct mbuf *, uint8_t, uint32_t, uint32_t);
 void	in6_proto_cksum_out(struct mbuf *, struct ifnet *);
 int	in6_localaddr(struct in6_addr *);
 int	in6_addrscope(struct in6_addr *);
