43e07668bd8e4b9b8db735e5bd6f18c1b63f6876
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-07-09 14:23:28 +0200

    cvs update

--- kern/kern_prot.c
+++ kern/kern_prot.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_prot.c,v 1.75 2018/06/22 13:33:30 visa Exp $	*/
+/*	$OpenBSD: kern_prot.c,v 1.76 2019/07/09 12:23:25 bluhm Exp $	*/
 /*	$NetBSD: kern_prot.c,v 1.33 1996/02/09 18:59:42 christos Exp $	*/
 
 /*
