56060ec30fc92294aa34f6f9f04968da440d5da2
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-08-02 00:17:22 +0200

    Use pool_get(namei) in sys_unveil().

--- kern/vfs_syscalls.c
+++ kern/vfs_syscalls.c
@@ -975,7 +975,7 @@ sys_unveil(struct proc *p, void *v, register_t *retval)
 		syscallarg(const char *) path;
 		syscallarg(const char *) permissions;
 	} */ *uap = v;
-	char pathname[MAXPATHLEN], *c;
+	char *pathname, *c;
 	struct nameidata nd;
 	size_t pathlen;
 	char permissions[5];
@@ -992,10 +992,11 @@ sys_unveil(struct proc *p, void *v, register_t *retval)
 	error = copyinstr(SCARG(uap, permissions), permissions,
 	    sizeof(permissions), NULL);
 	if (error)
-		return(error);
-	error = copyinstr(SCARG(uap, path), pathname, sizeof(pathname), &pathlen);
+		return (error);
+	pathname = pool_get(&namei_pool, PR_WAITOK);
+	error = copyinstr(SCARG(uap, path), pathname, MAXPATHLEN, &pathlen);
 	if (error)
-		return(error);
+		goto put;
 
 #ifdef KTRACE
 	if (KTRPOINT(p, KTR_STRUCT))
@@ -1061,6 +1062,8 @@ sys_unveil(struct proc *p, void *v, register_t *retval)
 	pool_put(&namei_pool, nd.ni_cnd.cn_pnbuf);
 end:
 	unveil_free_traversed_vnodes(&nd);
+put:
+	pool_put(&namei_pool, pathname);
 
 	return (error);
 }
