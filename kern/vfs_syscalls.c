26ff2faac08568a4258373768745aacfe8a746aa
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-08-04 10:42:32 +0200

    cvs commit

--- kern/vfs_syscalls.c
+++ kern/vfs_syscalls.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: vfs_syscalls.c,v 1.328 2019/08/02 08:12:35 bluhm Exp $	*/
+/*	$OpenBSD: vfs_syscalls.c,v 1.329 2019/08/04 08:42:29 bluhm Exp $	*/
 /*	$NetBSD: vfs_syscalls.c,v 1.71 1996/04/23 10:29:02 mycroft Exp $	*/
 
 /*
