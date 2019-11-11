59b64e4c0f9fb9e60d2a1693543a4b9703112f9b
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-11-11 01:43:21 +0100

    cvs update

--- net/if.c
+++ net/if.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if.c,v 1.591 2019/11/07 08:03:18 dlg Exp $	*/
+/*	$OpenBSD: if.c,v 1.593 2019/11/09 09:45:25 dlg Exp $	*/
 /*	$NetBSD: if.c,v 1.35 1996/05/07 05:26:04 thorpej Exp $	*/
 
 /*
@@ -230,7 +230,7 @@ int if_cloners_count;
 
 /* hooks should only be added, deleted, and run from a process context */
 struct mutex if_hooks_mtx = MUTEX_INITIALIZER(IPL_NONE);
-void 	if_hooks_run(struct task_list *);
+void	if_hooks_run(struct task_list *);
 
 struct timeout net_tick_to;
 void	net_tick(void *);
@@ -630,9 +630,7 @@ if_attach_common(struct ifnet *ifp)
 	ifp->if_iqs = ifp->if_rcv.ifiq_ifiqs;
 	ifp->if_niqs = 1;
 
-	ifp->if_addrhooks = malloc(sizeof(*ifp->if_addrhooks),
-	    M_TEMP, M_WAITOK);
-	TAILQ_INIT(ifp->if_addrhooks);
+	TAILQ_INIT(&ifp->if_addrhooks);
 	TAILQ_INIT(&ifp->if_linkstatehooks);
 	TAILQ_INIT(&ifp->if_detachhooks);
 
@@ -679,7 +677,7 @@ if_qstart_compat(struct ifqueue *ifq)
 	 * this provides compatability between the stack and the older
 	 * drivers by translating from the only queue they have
 	 * (ifp->if_snd) back to the interface and calling if_start.
- 	 */
+	 */
 
 	KERNEL_LOCK();
 	s = splnet();
@@ -1046,19 +1044,18 @@ if_netisr(void *unused)
 void
 if_hooks_run(struct task_list *hooks)
 {
-	struct task *t, *nt, cursor;
+	struct task *t, *nt;
+	struct task cursor = { .t_func = NULL };
 	void (*func)(void *);
 	void *arg;
 
-	/*
-	 * holding the NET_LOCK guarantees that concurrent if_hooks_run
-	 * calls can't happen, and they therefore can't try and call
-	 * each others cursors as actual hooks.
-	 */
-	NET_ASSERT_LOCKED();
-
 	mtx_enter(&if_hooks_mtx);
 	for (t = TAILQ_FIRST(hooks); t != NULL; t = nt) {
+		while (t->t_func == NULL) { /* skip cursors */
+			t = TAILQ_NEXT(t, t_entry);
+			if (t == NULL)
+				break;
+		}
 		func = t->t_func;
 		arg = t->t_arg;
 
@@ -1177,7 +1174,7 @@ if_detach(struct ifnet *ifp)
 		}
 	}
 
-	free(ifp->if_addrhooks, M_TEMP, sizeof(*ifp->if_addrhooks));
+	KASSERT(TAILQ_EMPTY(&ifp->if_addrhooks));
 	KASSERT(TAILQ_EMPTY(&ifp->if_linkstatehooks));
 	KASSERT(TAILQ_EMPTY(&ifp->if_detachhooks));
 
@@ -1369,7 +1366,7 @@ if_clone_attach(struct if_clone *ifc)
 	 * we are called at kernel boot by main(), when pseudo devices are
 	 * being attached. The main() is the only guy which may alter the
 	 * if_cloners. While system is running and main() is done with
-	 * initialization, the if_cloners becomes immutable. 
+	 * initialization, the if_cloners becomes immutable.
 	 */
 	KASSERT(pdevinit_done == 0);
 	LIST_INSERT_HEAD(&if_cloners, ifc, ifc_list);
@@ -2893,9 +2890,9 @@ if_getgrouplist(caddr_t data)
 			return (EINVAL);
 		bzero(&ifgrq, sizeof ifgrq);
 		strlcpy(ifgrq.ifgrq_group, ifg->ifg_group,
-                    sizeof(ifgrq.ifgrq_group));
+		    sizeof(ifgrq.ifgrq_group));
 		if ((error = copyout((caddr_t)&ifgrq, (caddr_t)ifgp,
-                    sizeof(struct ifg_req))))
+		    sizeof(struct ifg_req))))
 			return (error);
 		len -= sizeof(ifgrq);
 		ifgp++;
@@ -3100,7 +3097,7 @@ ifnewlladdr(struct ifnet *ifp)
 		ifa = &in6ifa_ifpforlinklocal(ifp, 0)->ia_ifa;
 		if (ifa) {
 			in6_purgeaddr(ifa);
-			dohooks(ifp->if_addrhooks, 0);
+			if_hooks_run(&ifp->if_addrhooks);
 			in6_ifattach(ifp);
 		}
 	}
@@ -3112,6 +3109,28 @@ ifnewlladdr(struct ifnet *ifp)
 		(*ifp->if_ioctl)(ifp, SIOCSIFFLAGS, (caddr_t)&ifrq);
 	}
 	splx(s);
+}
+
+void
+if_addrhook_add(struct ifnet *ifp, struct task *t)
+{
+	mtx_enter(&if_hooks_mtx);
+	TAILQ_INSERT_TAIL(&ifp->if_addrhooks, t, t_entry);
+	mtx_leave(&if_hooks_mtx);
+}
+
+void
+if_addrhook_del(struct ifnet *ifp, struct task *t)
+{
+	mtx_enter(&if_hooks_mtx);
+	TAILQ_REMOVE(&ifp->if_addrhooks, t, t_entry);
+	mtx_leave(&if_hooks_mtx);
+}
+
+void
+if_addrhooks_run(struct ifnet *ifp)
+{
+	if_hooks_run(&ifp->if_addrhooks);
 }
 
 int net_ticks;
