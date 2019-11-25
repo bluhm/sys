97e07ea70855fbc7942fb1ef81290e57ad6c93ca
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-11-25 12:43:50 +0100

    More debugging effort in efiboot.

--- arch/amd64/stand/efiboot/efiboot.c
+++ arch/amd64/stand/efiboot/efiboot.c
@@ -154,21 +154,33 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
 void
 efi_cleanup(void)
 {
-	int		 retry;
-	EFI_STATUS	 status;
+	int			 retry;
+	EFI_STATUS		 status;
+	EFI_MEMORY_DESCRIPTOR	*mem_map;
+	UINTN			 mem_map_size, map_key, desc_size;
+	UINT32			 desc_version;
 
 	/* retry once in case of failure */
 	for (retry = 5; retry >= 0; retry--) {
-		efi_memprobe_internal();	/* sync the current map */
-printf("efi_cleanup: BS %p, ExitBootServices %p, IH %p, mmap_key %llu\n",
-    BS, BS->ExitBootServices, IH, mmap_key);
-		status = EFI_CALL(BS->ExitBootServices, IH, mmap_key);
+		mem_map_size = 0;
+		status = EFI_CALL(BS->GetMemoryMap, &mem_map_size, NULL,
+		    &map_key, &desc_size, &desc_version);
+		if (status != EFI_BUFFER_TOO_SMALL)
+			panic("cannot get the size of memory map (%u)", status);
+		mem_map_size += 2 * desc_size;
+		mem_map = alloc(mem_map_size);
+		if (mem_map == NULL)
+			panic("alloc memory map");
+		status = EFI_CALL(BS->GetMemoryMap, &mem_map_size, mem_map,
+		    &map_key, &desc_size, &desc_version);
+		if (status != EFI_SUCCESS)
+			panic("cannot get the memory map (%u)", status);
+		status = EFI_CALL(BS->ExitBootServices, IH, map_key);
 		if (status == EFI_SUCCESS)
 			break;
-printf("ExitBootServices failed (%d)\n", status);
 		if (retry == 0) {
 for (volatile long long i = 0; i < 10000000000LL; i++) continue;
-			panic("ExitBootServices failed (%d)", status);
+			panic("ExitBootServices failed (%u)", status);
 		}
 	}
 }
@@ -348,14 +360,10 @@ efi_memprobe_internal(void)
 	    &mmver);
 	if (status != EFI_BUFFER_TOO_SMALL)
 		panic("cannot get the size of memory map");
-printf("siz %llu, mapkey %llu, mmsiz %llu, mmver %u\n",
-    siz, mapkey, mmsiz, mmver);
 	mm0 = alloc(siz);
 	status = EFI_CALL(BS->GetMemoryMap, &siz, mm0, &mapkey, &mmsiz, &mmver);
 	if (status != EFI_SUCCESS)
 		panic("cannot get the memory map");
-printf("siz %llu, mapkey %llu, mmsiz %llu, mmver %u\n",
-    siz, mapkey, mmsiz, mmver);
 	n = siz / mmsiz;
 	mmap_key = mapkey;
 
@@ -384,8 +392,6 @@ printf("siz %llu, mapkey %llu, mmsiz %llu, mmver %u\n",
 			 * XXX EfiMemoryMappedIOPortSpace EfiPalCode?
 			 */
 			bm0.type = BIOS_MAP_RES;
-printf("bm0 addr %llu, size %llu, type %u, Type %u\n",
-    bm0.addr, bm0.size, bm0.type, mm->Type);
 
 		for (bm = bios_memmap; bm->type != BIOS_MAP_END; bm++) {
 			if (bm->type != bm0.type)
@@ -414,7 +420,7 @@ printf("bm0 addr %llu, size %llu, type %u, Type %u\n",
 		    bm->addr / 1024 == extmem + 1024)
 			extmem += bm->size / 1024;
 	}
-//	free(mm0, siz);
+	free(mm0, siz);
 }
 
 /***********************************************************************
