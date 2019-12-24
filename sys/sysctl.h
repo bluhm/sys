a3ea8f49a6a8d8260f95bb44b3b4f6e3a8a13bff
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-12-24 14:14:02 +0100

    cvs commit

--- sys/sysctl.h
+++ sys/sysctl.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: sysctl.h,v 1.198 2019/12/11 07:30:09 guenther Exp $	*/
+/*	$OpenBSD: sysctl.h,v 1.199 2019/12/24 13:13:54 bluhm Exp $	*/
 /*	$NetBSD: sysctl.h,v 1.16 1996/04/09 20:55:36 cgd Exp $	*/
 
 /*
