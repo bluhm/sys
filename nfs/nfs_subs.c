a47616966086f366786f4f1e59244058a63d2548
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-12-24 22:54:51 +0100

    In nfs_clearcommit() use LIST_FOREACH_SAFE(mnt_vnodelist).

--- nfs/nfs_subs.c
+++ nfs/nfs_subs.c
@@ -1515,10 +1515,9 @@ nfs_clearcommit(struct mount *mp)
 
 	s = splbio();
 loop:
-	for (vp = LIST_FIRST(&mp->mnt_vnodelist); vp != NULL; vp = nvp) {
+	LIST_FOREACH_SAFE(vp, &mp->mnt_vnodelist, v_mntvnodes, nvp) {
 		if (vp->v_mount != mp)	/* Paranoia */
 			goto loop;
-		nvp = LIST_NEXT(vp, v_mntvnodes);
 		LIST_FOREACH_SAFE(bp, &vp->v_dirtyblkhd, b_vnbufs, nbp) {
 			if ((bp->b_flags & (B_BUSY | B_DELWRI | B_NEEDCOMMIT))
 			    == (B_DELWRI | B_NEEDCOMMIT))
