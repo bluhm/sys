fe58552b6cbd4302b1c6c33f877a321c1af40b25
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-10-27 22:55:59 +0100

    Pullup must modify mbuf in caller.

--- netinet/icmp6.h
+++ netinet/icmp6.h
@@ -594,7 +594,7 @@ struct mbuf	*icmp6_do_error(struct mbuf *, int, int, int);
 void		 icmp6_error(struct mbuf *, int, int, int);
 int		 icmp6_input(struct mbuf **, int *, int, int);
 void		 icmp6_fasttimo(void);
-int		 icmp6_reflect(struct mbuf *, size_t, struct sockaddr *);
+int		 icmp6_reflect(struct mbuf **, size_t, struct sockaddr *);
 void		 icmp6_prepare(struct mbuf *);
 void		 icmp6_redirect_input(struct mbuf *, int);
 void		 icmp6_redirect_output(struct mbuf *, struct rtentry *);
