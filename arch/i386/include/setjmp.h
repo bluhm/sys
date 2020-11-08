fc5c06ba48f3bbd5438081b33b7bf4a7ca717a5d
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-11-08 17:12:32 +0100

    Introduce setjmp _JB register offset macros.

--- arch/i386/include/setjmp.h
+++ arch/i386/include/setjmp.h
@@ -5,4 +5,15 @@
  * machine/setjmp.h: machine dependent setjmp-related information.
  */
 
-#define	_JBLEN	10		/* size, in longs, of a jmp_buf */
+#define _JB_EIP		0
+#define _JB_EBX		1
+#define _JB_ESP		2
+#define _JB_EBP		3
+#define _JB_ESI		4
+#define _JB_EDI		5
+#define _JB_SIGMASK	6
+#define _JB_SIGFLAG	7
+#define _JB_MXCSR	8
+#define _JB_FCW		9
+
+#define _JBLEN		10	/* size, in longs, of a jmp_buf */
