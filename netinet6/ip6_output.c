5ab6a421455abd59601b6b69fc2c26ffd3216399
Alexander Bluhm <alexander.bluhm@gmx.net>
2020-12-21 23:28:16 +0100

    Return EACCES if pf_test fails in IPv6.

--- netinet6/ip6_output.c
+++ netinet6/ip6_output.c
@@ -616,7 +616,7 @@ reroute:
 
 #if NPF > 0
 	if (pf_test(AF_INET6, PF_OUT, ifp, &m) != PF_PASS) {
-		error = EHOSTUNREACH;
+		error = EACCES;
 		m_freem(m);
 		goto done;
 	}
@@ -2773,7 +2773,7 @@ ip6_output_ipsec_send(struct tdb *tdb, struct mbuf *m, int tunalready, int fwd)
 	if ((encif = enc_getif(tdb->tdb_rdomain, tdb->tdb_tap)) == NULL ||
 	    pf_test(AF_INET6, fwd ? PF_FWD : PF_OUT, encif, &m) != PF_PASS) {
 		m_freem(m);
-		return EHOSTUNREACH;
+		return EACCES;
 	}
 	if (m == NULL)
 		return 0;
