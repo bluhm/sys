ec925b09e435d65130e8770cbd20704de0630b6e
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-01-11 15:35:45 +0100

    Only set user id in pflog id requested by pf.conf.

--- net/if_pflog.c
+++ net/if_pflog.c
@@ -244,7 +244,7 @@ pflog_packet(struct pf_pdesc *pd, u_int8_t reason, struct pf_rule *rm,
 	}
 	if (trigger->log & PF_LOG_SOCKET_LOOKUP && !pd->lookup.done)
 		pd->lookup.done = pf_socket_lookup(pd);
-	if (pd->lookup.done > 0) {
+	if (trigger->log & PF_LOG_SOCKET_LOOKUP && pd->lookup.done > 0) {
 		hdr.uid = pd->lookup.uid;
 		hdr.pid = pd->lookup.pid;
 	} else {
