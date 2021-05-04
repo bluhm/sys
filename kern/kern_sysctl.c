9d755f55323ccbb0995fe0ed9d6d94bb1f84d128
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-05-04 21:05:01 +0200

    cvs commit

--- kern/kern_sysctl.c
+++ kern/kern_sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sysctl.c,v 1.392 2021/05/01 16:18:28 gnezdo Exp $	*/
+/*	$OpenBSD: kern_sysctl.c,v 1.393 2021/05/04 19:04:56 bluhm Exp $	*/
 /*	$NetBSD: kern_sysctl.c,v 1.17 1996/05/20 17:49:05 mrg Exp $	*/
 
 /*-
