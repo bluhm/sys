f3aae3910703f6e0b7ca643a87f94f7c377ba2d1
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-08-09 19:03:12 +0200

    cvs commit

--- netinet/tcp_input.c
+++ netinet/tcp_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_input.c,v 1.369 2021/08/09 16:06:31 bluhm Exp $	*/
+/*	$OpenBSD: tcp_input.c,v 1.370 2021/08/09 17:03:08 bluhm Exp $	*/
 /*	$NetBSD: tcp_input.c,v 1.23 1996/02/13 23:43:44 christos Exp $	*/
 
 /*
