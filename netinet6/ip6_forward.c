b85f415506513322c409bea8ae9e28c20feb91b1
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-10-14 19:39:51 +0200

    cvs commit

--- netinet6/ip6_forward.c
+++ netinet6/ip6_forward.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip6_forward.c,v 1.100 2021/01/11 13:28:54 bluhm Exp $	*/
+/*	$OpenBSD: ip6_forward.c,v 1.101 2021/10/14 17:39:42 bluhm Exp $	*/
 /*	$KAME: ip6_forward.c,v 1.75 2001/06/29 12:42:13 jinmei Exp $	*/
 
 /*
