0e6bf1ae4eb200a1c1a9deccdc2eadefc91e7b66
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-11-01 10:19:14 +0100

    cvs commit

--- netinet/ipsec_input.c
+++ netinet/ipsec_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ipsec_input.c,v 1.189 2021/10/24 22:59:47 bluhm Exp $	*/
+/*	$OpenBSD: ipsec_input.c,v 1.190 2021/11/01 09:19:10 bluhm Exp $	*/
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr) and
