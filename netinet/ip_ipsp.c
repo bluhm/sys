34a69b2f2fec6228dc4c6069b69235611a6de192
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-11-26 20:24:44 +0100

    cvs commit

--- netinet/ip_ipsp.c
+++ netinet/ip_ipsp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_ipsp.c,v 1.255 2021/11/26 16:16:35 tobhe Exp $	*/
+/*	$OpenBSD: ip_ipsp.c,v 1.256 2021/11/26 19:24:41 bluhm Exp $	*/
 /*
  * The authors of this code are John Ioannidis (ji@tla.org),
  * Angelos D. Keromytis (kermit@csd.uch.gr),
