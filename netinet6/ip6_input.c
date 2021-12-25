51141dbe85e522110973c6d1c1d2a8ad802bfa27
Alexander Bluhm <alexander.bluhm@gmx.net>
2021-12-25 14:35:24 +0100

    cvs commit

--- netinet6/ip6_input.c
+++ netinet6/ip6_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip6_input.c,v 1.237 2021/06/03 04:47:54 dlg Exp $	*/
+/*	$OpenBSD: ip6_input.c,v 1.238 2021/12/25 13:35:17 bluhm Exp $	*/
 /*	$KAME: ip6_input.c,v 1.188 2001/03/29 05:34:31 itojun Exp $	*/
 
 /*
