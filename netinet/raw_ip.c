cac6ea0c90f7e2eb221c79710612102ff4970580
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-03-23 18:22:38 +0100

    cvs commit

--- netinet/raw_ip.c
+++ netinet/raw_ip.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: raw_ip.c,v 1.126 2022/03/22 18:17:30 bluhm Exp $	*/
+/*	$OpenBSD: raw_ip.c,v 1.127 2022/03/23 17:22:28 bluhm Exp $	*/
 /*	$NetBSD: raw_ip.c,v 1.25 1996/02/18 18:58:33 christos Exp $	*/
 
 /*
