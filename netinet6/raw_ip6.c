1789ab69fc6844bd040249ad3656c18aec1c81b5
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-03-23 01:16:10 +0100

    cvs commit

--- netinet6/raw_ip6.c
+++ netinet6/raw_ip6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: raw_ip6.c,v 1.146 2022/03/22 18:27:21 bluhm Exp $	*/
+/*	$OpenBSD: raw_ip6.c,v 1.147 2022/03/23 00:16:07 bluhm Exp $	*/
 /*	$KAME: raw_ip6.c,v 1.69 2001/03/04 15:55:44 itojun Exp $	*/
 
 /*
