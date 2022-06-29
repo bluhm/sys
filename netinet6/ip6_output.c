607c12a237cb1262779922c76e83f9ebdd6c3bfe
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-06-29 13:40:33 +0200

    Use mbuf pointer in ip6_process_hopopts() and ip6_unknown_opt().

--- netinet6/ip6_output.c
+++ netinet6/ip6_output.c
@@ -619,7 +619,7 @@ reroute:
 		u_int32_t plen = 0; /* no more than 1 jumbo payload option! */
 
 		m->m_pkthdr.ph_ifidx = ifp->if_index;
-		if (ip6_process_hopopts(m, (u_int8_t *)(hbh + 1),
+		if (ip6_process_hopopts(&m, (u_int8_t *)(hbh + 1),
 		    ((hbh->ip6h_len + 1) << 3) - sizeof(struct ip6_hbh),
 		    &rtalert, &plen) < 0) {
 			/* m was already freed at this point */
