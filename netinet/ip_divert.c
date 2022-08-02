eb03cd748a1f6774dd3119fc6bedf497bc4a57c1
Alexander Bluhm <alexander.bluhm@gmx.net>
2022-08-02 13:24:36 +0200

    Remove kernel lock in divert_packet().

--- netinet/ip_divert.c
+++ netinet/ip_divert.c
@@ -222,21 +222,11 @@ divert_packet(struct mbuf *m, int dir, u_int16_t divert_port)
 	}
 
 	so = inp->inp_socket;
-	/*
-	 * XXXSMP sbappendaddr() is not MP safe and this function is called
-	 * from pf with shared netlock.  To call only one sbappendaddr() from
-	 * divert_packet(), protect it with kernel lock.  All other places
-	 * call sbappendaddr() with exclusive net lock.  This blocks
-	 * divert_packet() as we have the shared lock.
-	 */
-	KERNEL_LOCK();
 	if (sbappendaddr(so, &so->so_rcv, sintosa(&sin), m, NULL) == 0) {
-		KERNEL_UNLOCK();
 		divstat_inc(divs_fullsock);
 		goto bad;
 	}
 	sorwakeup(inp->inp_socket);
-	KERNEL_UNLOCK();
 
 	in_pcbunref(inp);
 	return;
