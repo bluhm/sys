b630e33e9400a07718393c9183cf0d77f934593e
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-03-09 00:17:11 +0100

    cvs commit

--- netinet/ip_input.c
+++ netinet/ip_input.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_input.c,v 1.381 2022/08/29 14:43:56 bluhm Exp $	*/
+/*	$OpenBSD: ip_input.c,v 1.382 2023/03/08 23:17:02 bluhm Exp $	*/
 /*	$NetBSD: ip_input.c,v 1.30 1996/03/16 23:53:58 christos Exp $	*/
 
 /*
