631e545ea2ab14979f8af58ee4953b4dfec241bf
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-06-13 21:34:22 +0200

    cvs commit

--- netinet6/ip6_output.c
+++ netinet6/ip6_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip6_output.c,v 1.277 2023/05/22 16:08:34 bluhm Exp $	*/
+/*	$OpenBSD: ip6_output.c,v 1.278 2023/06/13 19:34:12 bluhm Exp $	*/
 /*	$KAME: ip6_output.c,v 1.172 2001/03/25 09:55:56 itojun Exp $	*/
 
 /*
