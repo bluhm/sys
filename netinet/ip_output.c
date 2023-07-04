b7f304d38208f98e2c6b0f476fae8834f295e83d
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-07-04 13:49:00 +0300

    cvs commit

--- netinet/ip_output.c
+++ netinet/ip_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_output.c,v 1.388 2023/05/22 16:08:34 bluhm Exp $	*/
+/*	$OpenBSD: ip_output.c,v 1.389 2023/07/04 10:48:19 bluhm Exp $	*/
 /*	$NetBSD: ip_output.c,v 1.28 1996/02/13 23:43:07 christos Exp $	*/
 
 /*
--- netinet/tcp_output.c
+++ netinet/tcp_output.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: tcp_output.c,v 1.138 2023/05/15 16:34:56 bluhm Exp $	*/
+/*	$OpenBSD: tcp_output.c,v 1.139 2023/07/04 10:48:19 bluhm Exp $	*/
 /*	$NetBSD: tcp_output.c,v 1.16 1997/06/03 16:17:09 kml Exp $	*/
 
 /*
