4b8695b6dd3b04c41e975ab19cce2c3393fa81c2
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-07-27 20:38:08 +0200

    cvs update

--- arch/amd64/include/cpu.h
+++ arch/amd64/include/cpu.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: cpu.h,v 1.155 2023/07/04 17:29:32 cheloha Exp $	*/
+/*	$OpenBSD: cpu.h,v 1.157 2023/07/25 18:16:19 cheloha Exp $	*/
 /*	$NetBSD: cpu.h,v 1.1 2003/04/26 18:39:39 fvdl Exp $	*/
 
 /*-
@@ -208,6 +208,7 @@ struct cpu_info {
 	u_int64_t		ci_hz_aperf;
 #if defined(GPROF) || defined(DDBPROF)
 	struct gmonparam	*ci_gmon;
+	struct clockintr	*ci_gmonclock;
 #endif
 	u_int32_t	ci_vmm_flags;
 #define	CI_VMM_VMX	(1 << 0)
@@ -403,7 +404,9 @@ void	cpu_reset(void);
 void	x86_64_proc0_tss_ldt_init(void);
 void	cpu_proc_fork(struct proc *, struct proc *);
 int	amd64_pa_used(paddr_t);
+#define	cpu_idle_enter()	do { /* nothing */ } while (0)
 extern void (*cpu_idle_cycle_fcn)(void);
+#define	cpu_idle_leave()	do { /* nothing */ } while (0)
 
 struct region_descriptor;
 void	lgdt(struct region_descriptor *);
