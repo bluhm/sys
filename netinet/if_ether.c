3fc8ec3ff07bcaf6aa79de6d5cf1bf7032090bce
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-11-09 22:45:42 +0100

    cvs commit

--- netinet/if_ether.c
+++ netinet/if_ether.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_ether.c,v 1.265 2023/05/12 12:40:49 bluhm Exp $	*/
+/*	$OpenBSD: if_ether.c,v 1.266 2023/11/09 21:45:18 bluhm Exp $	*/
 /*	$NetBSD: if_ether.c,v 1.31 1996/05/11 12:59:58 mycroft Exp $	*/
 
 /*
