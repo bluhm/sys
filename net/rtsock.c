f5358f6a1c1b1e885002d0e137335704a224a1b0
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-11-12 17:10:51 +0100

    cvs commit

--- net/rtsock.c
+++ net/rtsock.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: rtsock.c,v 1.370 2023/09/16 09:33:27 mpi Exp $	*/
+/*	$OpenBSD: rtsock.c,v 1.371 2023/11/12 16:10:46 bluhm Exp $	*/
 /*	$NetBSD: rtsock.c,v 1.18 1996/03/29 00:32:10 cgd Exp $	*/
 
 /*
