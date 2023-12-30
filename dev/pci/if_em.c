40f2b9acbd79f96f604fb02dcb339c942035a0a0
Alexander Bluhm <alexander.bluhm@gmx.net>
2023-12-30 13:44:49 +0100

    cvs commit

--- dev/pci/if_em.c
+++ dev/pci/if_em.c
@@ -31,7 +31,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 ***************************************************************************/
 
-/* $OpenBSD: if_em.c,v 1.368 2023/12/03 00:19:25 jsg Exp $ */
+/* $OpenBSD: if_em.c,v 1.369 2023/12/30 12:44:43 bluhm Exp $ */
 /* $FreeBSD: if_em.c,v 1.46 2004/09/29 18:28:28 mlaier Exp $ */
 
 #include <dev/pci/if_em.h>
