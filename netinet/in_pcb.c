cfbaa7265b28e0a32c3b15d56702fe86be21b353
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-02-02 16:39:28 +0100

    cvs commit

--- netinet/in_pcb.c
+++ netinet/in_pcb.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: in_pcb.c,v 1.288 2024/01/31 12:27:57 bluhm Exp $	*/
+/*	$OpenBSD: in_pcb.c,v 1.289 2024/02/02 15:39:23 bluhm Exp $	*/
 /*	$NetBSD: in_pcb.c,v 1.25 1996/02/13 23:41:53 christos Exp $	*/
 
 /*
