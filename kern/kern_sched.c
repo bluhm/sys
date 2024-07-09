d4a84e015ea8a257ca754e345c8841db93607b5c
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-07-09 13:33:42 +0200

    cvs update

--- kern/kern_sched.c
+++ kern/kern_sched.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sched.c,v 1.96 2024/06/03 12:48:25 claudio Exp $	*/
+/*	$OpenBSD: kern_sched.c,v 1.100 2024/07/09 08:44:36 claudio Exp $	*/
 /*
  * Copyright (c) 2007, 2008 Artur Grabowski <art@openbsd.org>
  *
@@ -213,8 +213,21 @@ void
 sched_exit(struct proc *p)
 {
 	struct schedstate_percpu *spc = &curcpu()->ci_schedstate;
+	struct timespec ts;
 
 	LIST_INSERT_HEAD(&spc->spc_deadproc, p, p_hash);
+
+	/* update the tu_runtime one last time */
+	nanouptime(&ts);
+	if (timespeccmp(&ts, &spc->spc_runtime, <))
+		timespecclear(&ts);
+	else
+		timespecsub(&ts, &spc->spc_runtime, &ts);
+
+	/* add the time counts for this thread */
+	tu_enter(&p->p_tu);
+	timespecadd(&p->p_tu.tu_runtime, &ts, &p->p_tu.tu_runtime);
+	tu_leave(&p->p_tu);
 
 	KERNEL_ASSERT_LOCKED();
 	sched_toidle();
@@ -248,8 +261,9 @@ sched_toidle(void)
 	idle->p_stat = SRUN;
 
 	uvmexp.swtch++;
-	TRACEPOINT(sched, off__cpu, idle->p_tid + THREAD_PID_OFFSET,
-	    idle->p_p->ps_pid);
+	if (curproc != NULL)
+		TRACEPOINT(sched, off__cpu, idle->p_tid + THREAD_PID_OFFSET,
+		    idle->p_p->ps_pid);
 	cpu_switchto(NULL, idle);
 	panic("cpu_switchto returned");
 }
@@ -633,6 +647,14 @@ sched_peg_curproc(struct cpu_info *ci)
 	SCHED_UNLOCK();
 }
 
+void
+sched_unpeg_curproc(void)
+{
+	struct proc *p = curproc;
+
+	atomic_clearbits_int(&p->p_flag, P_CPUPEG);
+}
+
 #ifdef MULTIPROCESSOR
 
 void
@@ -699,7 +721,7 @@ sched_barrier_task(void *arg)
 
 	sched_peg_curproc(ci);
 	cond_signal(&sb->cond);
-	atomic_clearbits_int(&curproc->p_flag, P_CPUPEG);
+	sched_unpeg_curproc();
 }
 
 void
