51b6238e756b4b0c5c5b160c668f0e35c634af35
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-08-19 11:38:26 +0200

    Print when running out of bounce buffers.

--- arch/amd64/amd64/bus_dma.c
+++ arch/amd64/amd64/bus_dma.c
@@ -725,9 +725,13 @@ _bus_dmamap_load_buffer(bus_dma_tag_t t, bus_dmamap_t map, void *buf,
 			    curaddr);
 
 		if (use_bounce_buffer) {
-			if (page >= map->_dm_npages)
+			if (page >= map->_dm_npages) {
+				printf("%s: "
+				    "page %d, _dm_nused %d, _dm_npages %d\n",
+				    __func__,
+				    page, map->_dm_nused, map->_dm_npages);
 				return (ENOMEM);
-
+			}
 			off = vaddr & PAGE_MASK;
 			pg = map->_dm_pages[page];
 			curaddr = VM_PAGE_TO_PHYS(pg) + off;
