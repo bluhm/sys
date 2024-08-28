b73f7f74e66a8571eb92b492237133cb5aeb204b
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-08-28 20:22:01 +0200

    cvs commit

--- arch/amd64/amd64/bus_dma.c
+++ arch/amd64/amd64/bus_dma.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: bus_dma.c,v 1.57 2024/08/22 11:36:24 bluhm Exp $	*/
+/*	$OpenBSD: bus_dma.c,v 1.58 2024/08/28 18:21:15 bluhm Exp $	*/
 /*	$NetBSD: bus_dma.c,v 1.3 2003/05/07 21:33:58 fvdl Exp $	*/
 
 /*-
