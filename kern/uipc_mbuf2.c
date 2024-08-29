67e56a9e0e2250dea0345367756a6a0e6832a22c
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-08-29 18:42:36 +0200

    cvs commit

--- kern/uipc_mbuf2.c
+++ kern/uipc_mbuf2.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_mbuf2.c,v 1.46 2024/08/29 10:44:40 bluhm Exp $	*/
+/*	$OpenBSD: uipc_mbuf2.c,v 1.47 2024/08/29 16:42:30 bluhm Exp $	*/
 /*	$KAME: uipc_mbuf2.c,v 1.29 2001/02/14 13:42:10 itojun Exp $	*/
 /*	$NetBSD: uipc_mbuf.c,v 1.40 1999/04/01 00:23:25 thorpej Exp $	*/
 
