a6de96f591683b0ea811030a0fa98a70e9d5e5cb
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-11-20 14:37:00 +0100

    cvs commit

--- dev/ic/psp.c
+++ dev/ic/psp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: psp.c,v 1.14 2024/11/10 06:51:59 jsg Exp $ */
+/*	$OpenBSD: psp.c,v 1.15 2024/11/20 13:36:55 bluhm Exp $ */
 
 /*
  * Copyright (c) 2023, 2024 Hans-Joerg Hoexer <hshoexer@genua.de>
