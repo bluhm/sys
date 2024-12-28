71088a32cebdd41017e93c1dc7f3b187355781f3
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-12-28 13:43:57 +0100

    Make tcp_do_tso atomic.

--- netinet/tcp_var.h
+++ netinet/tcp_var.h
@@ -688,7 +688,7 @@ extern	struct pool sackhl_pool;
 extern	int tcp_sackhole_limit;	/* max entries for tcp sack queues */
 extern	int tcp_do_ecn;		/* RFC3168 ECN enabled/disabled? */
 extern	int tcp_do_rfc3390;	/* [a] RFC3390 Increasing TCP Initial Window */
-extern	int tcp_do_tso;		/* enable TSO for TCP output packets */
+extern	int tcp_do_tso;		/* [a] enable TSO for TCP output packets */
 
 extern	struct pool tcpqe_pool;
 extern	int tcp_reass_limit;	/* max entries for tcp reass queues */
