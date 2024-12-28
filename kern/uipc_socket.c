cfa2cae3e37c68486863ee8ab59705890342185d
Alexander Bluhm <alexander.bluhm@gmx.net>
2024-12-28 22:47:50 +0100

    cvs update

--- kern/uipc_socket.c
+++ kern/uipc_socket.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: uipc_socket.c,v 1.348 2024/12/26 10:24:57 bluhm Exp $	*/
+/*	$OpenBSD: uipc_socket.c,v 1.350 2024/12/27 13:08:11 mvs Exp $	*/
 /*	$NetBSD: uipc_socket.c,v 1.21 1996/02/04 02:17:52 christos Exp $	*/
 
 /*
