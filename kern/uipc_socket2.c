3cf5602cd19c83ac6ffad7169bd6ebdbfa8a8a35
Alexander Bluhm <alexander.bluhm@gmx.net>
2025-01-16 22:03:08 +0100

    Socket queue 0 is not used by internet sockets, put assert.

--- kern/uipc_socket2.c
+++ kern/uipc_socket2.c
@@ -100,21 +100,17 @@ soisconnected(struct socket *so)
 	so->so_state |= SS_ISCONNECTED;
 
 	if (head != NULL && so->so_onq == &head->so_q0) {
-		int persocket = solock_persocket(so);
+		KASSERT(solock_persocket(so));
 
-		if (persocket) {
-			soref(head);
+		soref(head);
+		sounlock(so);
+		solock(head);
+		solock(so);
 
-			sounlock(so);
-			solock(head);
-			solock(so);
-
-			if (so->so_onq != &head->so_q0) {
-				sounlock(head);
-				sorele(head);
-				return;
-			}
-
+		if (so->so_onq != &head->so_q0) {
+			sounlock(head);
+			sorele(head);
+			return;
 		}
 
 		soqremque(so, 0);
@@ -122,10 +118,8 @@ soisconnected(struct socket *so)
 		sorwakeup(head);
 		wakeup_one(&head->so_timeo);
 
-		if (persocket) {
-			sounlock(head);
-			sorele(head);
-		}
+		sounlock(head);
+		sorele(head);
 	} else {
 		wakeup(&so->so_timeo);
 		sorwakeup(so);
