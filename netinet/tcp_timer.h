b540baf4c0aa105681b5eaaea11d4bcb4afcdcab
Alexander Bluhm <alexander.bluhm@gmx.net>
2025-05-25 22:42:34 +0900

    Kill TCP reaper.

--- netinet/tcp_timer.h
+++ netinet/tcp_timer.h
@@ -164,8 +164,5 @@ extern int tcp_keepidle_sec;	/* [a] copy of above in seconds for sysctl */
 extern int tcp_keepintvl_sec;	/* [a] copy of above in seconds for sysctl */
 extern int tcp_ttl;		/* time to live for TCP segs */
 extern const int tcp_backoff[];
-
-void	tcp_timer_reaper(void *);
-
 #endif /* _KERNEL */
 #endif /* _NETINET_TCP_TIMER_H_ */
