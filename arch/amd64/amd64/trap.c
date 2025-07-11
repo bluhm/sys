90bcdbd2622884d274f826605bfc3dee1ceac667
Alexander Bluhm <alexander.bluhm@gmx.net>
2025-07-11 22:04:23 +0200

    cvs commit

--- arch/amd64/amd64/trap.c
+++ arch/amd64/amd64/trap.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: trap.c,v 1.110 2025/07/02 21:28:46 bluhm Exp $	*/
+/*	$OpenBSD: trap.c,v 1.111 2025/07/11 20:04:20 bluhm Exp $	*/
 /*	$NetBSD: trap.c,v 1.2 2003/05/04 23:51:56 fvdl Exp $	*/
 
 /*-
