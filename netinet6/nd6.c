89e3a59fead0a09079bee392d7739c4e4a9f443c
Alexander Bluhm <alexander.bluhm@gmx.net>
2025-08-04 23:51:06 +0200

    cvs commit

--- netinet6/nd6.c
+++ netinet6/nd6.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: nd6.c,v 1.298 2025/08/04 21:25:10 bluhm Exp $	*/
+/*	$OpenBSD: nd6.c,v 1.299 2025/08/04 21:50:59 bluhm Exp $	*/
 /*	$KAME: nd6.c,v 1.280 2002/06/08 19:52:07 itojun Exp $	*/
 
 /*
