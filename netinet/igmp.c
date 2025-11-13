97af315977f983784ae21e9d9b070c77d4e2358a
Alexander Bluhm <alexander.bluhm@gmx.net>
2025-11-13 16:50:38 +0100

    cvs commit

--- netinet/igmp.c
+++ netinet/igmp.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: igmp.c,v 1.90 2025/11/12 11:37:08 bluhm Exp $	*/
+/*	$OpenBSD: igmp.c,v 1.91 2025/11/13 15:49:50 bluhm Exp $	*/
 /*	$NetBSD: igmp.c,v 1.15 1996/02/13 23:41:25 christos Exp $	*/
 
 /*
