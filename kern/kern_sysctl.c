adb4b605aa90b34d8bfa635a8952581169e0d74b
Alexander Bluhm <alexander.bluhm@gmx.net>
2026-02-03 21:05:12 +0100

    cvs commit

--- kern/kern_sysctl.c
+++ kern/kern_sysctl.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: kern_sysctl.c,v 1.483 2025/09/23 08:00:48 mpi Exp $	*/
+/*	$OpenBSD: kern_sysctl.c,v 1.484 2026/02/03 20:05:07 bluhm Exp $	*/
 /*	$NetBSD: kern_sysctl.c,v 1.17 1996/05/20 17:49:05 mrg Exp $	*/
 
 /*-
