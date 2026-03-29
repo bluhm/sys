05ff53d4c136908a0cd7bc328412f4724e21d602
Alexander Bluhm <alexander.bluhm@gmx.net>
2026-03-29 19:58:59 +0200

    cvs update

--- dev/pci/if_iwx.c
+++ dev/pci/if_iwx.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: if_iwx.c,v 1.223 2026/03/14 15:37:44 stsp Exp $	*/
+/*	$OpenBSD: if_iwx.c,v 1.225 2026/03/26 15:39:04 stsp Exp $	*/
 
 /*
  * Copyright (c) 2014, 2016 genua gmbh <info@genua.de>
@@ -8431,16 +8431,20 @@ iwx_rs_init_v3(struct iwx_softc *sc, struct iwx_node *in)
 		cfg_cmd.mode = IWX_TLC_MNG_MODE_VHT;
 		cfg_cmd.ht_rates[IWX_TLC_NSS_1][IWX_TLC_MCS_PER_BW_80] =
 		    htole16(iwx_rs_vht_rates(sc, ni, 1));
-		cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
-		    htole16(iwx_rs_vht_rates(sc, ni, 2));
+		if (iwx_mimo_enabled(sc)) {
+			cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
+			    htole16(iwx_rs_vht_rates(sc, ni, 2));
+		}
 	} else if (ni->ni_flags & IEEE80211_NODE_HT) {
 		cfg_cmd.mode = IWX_TLC_MNG_MODE_HT;
 		cfg_cmd.ht_rates[IWX_TLC_NSS_1][IWX_TLC_MCS_PER_BW_80] =
 		    htole16(iwx_rs_ht_rates(sc, ni,
 		    IEEE80211_HT_RATESET_SISO));
-		cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
-		    htole16(iwx_rs_ht_rates(sc, ni,
-		    IEEE80211_HT_RATESET_MIMO2));
+		if (iwx_mimo_enabled(sc)) {
+			cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
+			    htole16(iwx_rs_ht_rates(sc, ni,
+			    IEEE80211_HT_RATESET_MIMO2));
+		}
 	} else
 		cfg_cmd.mode = IWX_TLC_MNG_MODE_NON_HT;
 
@@ -8506,16 +8510,20 @@ iwx_rs_init_v4(struct iwx_softc *sc, struct iwx_node *in)
 		cfg_cmd.mode = IWX_TLC_MNG_MODE_VHT;
 		cfg_cmd.ht_rates[IWX_TLC_NSS_1][IWX_TLC_MCS_PER_BW_80] =
 		    htole16(iwx_rs_vht_rates(sc, ni, 1));
-		cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
-		    htole16(iwx_rs_vht_rates(sc, ni, 2));
+		if (iwx_mimo_enabled(sc)) {
+			cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
+			    htole16(iwx_rs_vht_rates(sc, ni, 2));
+		}
 	} else if (ni->ni_flags & IEEE80211_NODE_HT) {
 		cfg_cmd.mode = IWX_TLC_MNG_MODE_HT;
 		cfg_cmd.ht_rates[IWX_TLC_NSS_1][IWX_TLC_MCS_PER_BW_80] =
 		    htole16(iwx_rs_ht_rates(sc, ni,
 		    IEEE80211_HT_RATESET_SISO));
-		cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
-		    htole16(iwx_rs_ht_rates(sc, ni,
-		    IEEE80211_HT_RATESET_MIMO2));
+		if (iwx_mimo_enabled(sc)) {
+			cfg_cmd.ht_rates[IWX_TLC_NSS_2][IWX_TLC_MCS_PER_BW_80] =
+			    htole16(iwx_rs_ht_rates(sc, ni,
+			    IEEE80211_HT_RATESET_MIMO2));
+		}
 	} else
 		cfg_cmd.mode = IWX_TLC_MNG_MODE_NON_HT;
 
@@ -10991,6 +10999,22 @@ iwx_rx_pkt(struct iwx_softc *sc, struct iwx_rx_data *data, struct mbuf_list *ml)
 			if (status == 1 && start == 0 &&
 			    conf_id == IWX_SESSION_PROTECT_CONF_ASSOC)
 				sc->sc_flags &= ~IWX_FLAG_TE_ACTIVE;
+			break;
+		}
+
+		case IWX_WIDE_ID(IWX_MAC_CONF_GROUP,
+		    IWX_CHANNEL_SWITCH_START_NOTIF): {
+			if (sc->sc_ic.ic_opmode != IEEE80211_M_STA ||
+			    sc->sc_ic.ic_state != IEEE80211_S_RUN)
+				break;
+
+			if (ifp->if_flags & IFF_DEBUG)
+				printf("%s: firmware channel switch "
+				    "notification 0x%x\n",
+				    DEVNAME(sc), code);
+
+			if ((sc->sc_flags & IWX_FLAG_SHUTDOWN) == 0)
+				task_add(systq, &sc->init_task);
 			break;
 		}
 
