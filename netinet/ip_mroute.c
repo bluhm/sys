ba5709d12dc87f2668b197c4f37b44942fd8265d
Alexander Bluhm <alexander.bluhm@gmx.net>
2026-06-23 21:09:44 +0200

    cvs commit

--- netinet/ip_mroute.c
+++ netinet/ip_mroute.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: ip_mroute.c,v 1.151 2026/06/23 18:50:43 bluhm Exp $	*/
+/*	$OpenBSD: ip_mroute.c,v 1.152 2026/06/23 19:09:37 bluhm Exp $	*/
 /*	$NetBSD: ip_mroute.c,v 1.85 2004/04/26 01:31:57 matt Exp $	*/
 
 /*
